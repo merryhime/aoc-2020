f47c3302119a46eb0bd16243e30997c7f2a18c87
Merry <git@mary.rs>
2024-02-25 22:38:52 +0000

    day 06

---
+++ src/bin/day-06.rs
@@ -0,0 +1,52 @@
+use counter::Counter;
+use itertools::Itertools;
+use std::fs;
+
+fn count_qns_in_group(s: &str) -> usize {
+    s.chars()
+        .filter(|c| char::is_ascii_alphabetic(c))
+        .unique()
+        .count()
+}
+
+fn part1(s: &str) -> usize {
+    let re = regex::Regex::new(r"\r?\n\r?\n").unwrap();
+    re.split(s).map(count_qns_in_group).sum()
+}
+
+fn count_occurances_in_group(s: &str) -> usize {
+    let c = s
+        .chars()
+        .filter(|c| char::is_ascii_alphabetic(c))
+        .collect::<Counter<_>>();
+    let num = s.lines().count();
+    c.iter().filter(|(_, &v)| v == num).count()
+}
+
+fn part2(s: &str) -> usize {
+    let re = regex::Regex::new(r"\r?\n\r?\n").unwrap();
+    re.split(s).map(count_occurances_in_group).sum()
+}
+
+fn main() {
+    let example1 = "abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+
+    println!("{}", part1(example1));
+    println!("{}", part1(&fs::read_to_string("inputs/06").unwrap()));
+    println!("{}", part2(example1));
+    println!("{}", part2(&fs::read_to_string("inputs/06").unwrap()));
+}
